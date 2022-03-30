b5c2e59ba6c133d76bfe673b56803272b05d489e
Wang Renzhi <renzhi.wang@intel.com>
2022-03-30 16:24:29 +0800

    Change for icv task4 data

---
+++ convert_task4_label.py
@@ -0,0 +1,36 @@
+from textwrap import indent
+from dict2xml import dict2xml
+import json
+import os
+import shutil
+from tqdm import tqdm
+
+if __name__=='__main__':
+    data_root = os.path.join(os.getcwd(),'..','task4_train')
+    anno_ori_folder = os.path.join(data_root,'labels')
+    anno_convert_folder = os.path.join(data_root,'labels_xml')
+    if os.path.exists(anno_convert_folder):
+        shutil.rmtree(anno_convert_folder)
+    os.mkdir(anno_convert_folder)
+    for anno_file in tqdm(os.listdir(anno_ori_folder)):
+        with open(os.path.join(anno_ori_folder,anno_file),'r') as af:
+            anno_data = json.load(af)
+        new_anno = {
+            'filename': anno_file,
+            'folder': 'images',
+            'path': os.path.join('images', anno_data['info']['image_name'])
+        }
+        for i,box in enumerate(anno_data['annotation']):
+            bbox = box['bbox']
+            new_anno[f'object_{i}']={
+                'name': box['category'],
+                'bndbox':{
+                    'xmin': int(bbox[0] - bbox[2]/2),
+                    'ymin': int(bbox[1] - bbox[3]/2),
+                    'xmax': int(bbox[0] + bbox[2]/2),
+                    'ymax': int(bbox[1] + bbox[3]/2)
+                }
+            }
+        with open(os.path.join(anno_convert_folder,anno_file.replace('json','xml')),'w') as f:
+            xml = dict2xml(new_anno,wrap='annotation',indent="    ")
+            f.write(xml)
